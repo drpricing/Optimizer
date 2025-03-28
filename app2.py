2e421935e06b755e909761a5cf437358d1a704e2
drpricing <yian.yang@outlook.com>
2025-03-28 08:44:17 +0100

    Update app2.py

--- app2.py
+++ app2.py
@@ -50,7 +50,7 @@ for path in file_paths:
 # Streamlit UI
 st.title("💬 Dr. Pricing Talks")
 st.write("Welcome to Dr. Pricing's ChatBot! Please describe your pricing challenge below. Enjoy while it lasts! (:")
-uploaded_files = st.file_uploader("Upload files", accept_multiple_files=True)
+uploaded_files = st.file_uploader("Upload files here", accept_multiple_files=True)
 
 if uploaded_files:
     for uploaded_file in uploaded_files:
